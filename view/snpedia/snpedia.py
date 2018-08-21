f652360e21f2bf248b6e088c1a2a28fca1b7bede
hp910219 <792089975@qq.com>
2018-08-21 15:59:05 +0800

    first commit

--- view/snpedia/snpedia.py
+++ view/snpedia/snpedia.py
@@ -24,17 +24,28 @@ if __name__ == "__main__":
 
 
 def get_items(page):
-
-    json_path = 'jsons/page_%d.json' % page
+    json_path = 'items/page_%d.json' % page
     if os.path.exists(json_path):
         return read_data(json_path)
     url = 'https://www.snpedia.com/index.php?title=Special:Ask&offset=%d' % page
     url += '&limit=500&q=%5B%5BCategory%3AIs+a+genotype%5D%5D&p=mainlabel%3D%2Fformat%3Dtable&po=%3FMagnitude%0A%3FRepute%0A%3FSummary%0A&sort=magnitude&order=desc'
-    'https://www.snpedia.com/index.php/Special:Ask/-5B-5BCategory:Is-20a-20snp-5D-5D/-3FMax-20Magnitude/-3FSummary/mainlabel%3D/limit%3D10/order%3Ddesc/sort%3DMax-20Magnitude/offset%3D10/format%3Dtable'
+    items, data1, len_trs = get_table_datas(url, 0, base='')
+    if len(items) > 0:
+        write_data(items, 'items/page_%d.json' % page)
+        # write_data(items, 'items/page_%d.xlsx' % page)
+    return items
+
+
+def get_items2(page):
+    json_path = 'jsons/page_%d.json' % page
+    if os.path.exists(json_path):
+        return read_data(json_path)
+    url = 'https://www.snpedia.com/index.php?title=Special:Ask&offset=%d' % page
+    url += '&limit=500&q=%5B%5BCategory%3AIs+a+snp%5D%5D&p=mainlabel%3D%2Fformat%3Dtable&po=%3FMax+Magnitude%0A%3FSummary%0A&sort=Max+Magnitude&order=desc'
     items, data1, len_trs = get_table_datas(url, 0, base='')
 
     if len(items) > 0:
-        write_data(items, 'items/page_%d.json' % page)
+        write_data(items, json_path)
         # write_data(items, 'items/page_%d.xlsx' % page)
     return items
 
@@ -47,19 +58,40 @@ def uniq_list(items):
     return items1
 
 
-n = 1
-limit = 100000
-total = n * limit / 500
-items = []
-items2 = []
-for i in range(total):
-    items1 = get_items(i * 500)
-    if items1 not in items:
-        items.append(items1)
-        items2 += items1
-    else:
+def main1():
+    n = 1
+    limit = 100000
+    total = n * limit / 500
+    items = []
+    items2 = []
+    for i in range(total):
+        items1 = get_items(i * 500)
+        if items1 not in items:
+            items.append(items1)
+            items2 += items1
+        else:
+            msg = format_record('record%d' % (i * 500), i + 1, total)
+            print msg, get_time(), items.index(items1)
+    print len(items2)
+    write_data(items2, 'results/snpedia.csv')
+    write_data(items2, 'results/snpedia.json')
+
+
+def main2():
+    n = 2
+    limit = 100000
+    total = n * limit / 500
+    items = []
+    items2 = []
+    for i in range(total):
+        items1 = get_items2(i * 500)
+        if items1 not in items:
+            items.append(items1)
+            items2 += items1
         msg = format_record('record%d' % (i * 500), i + 1, total)
         print msg, get_time(), items.index(items1)
-print len(items2)
-write_data(items2, 'results/snpedia.csv')
-write_data(items2, 'results/snpedia.json')
+    print len(items2)
+    write_data(items2, 'results/snpedia2.csv')
+    write_data(items2, 'results/snpedia2.json')
+
+main2()
